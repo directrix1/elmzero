75b88c42ab8e424ac0fa3a1146effed19d158571
Edward Flick <directrix1@gmail.com>
2017-01-17 12:38:52 -0600

    Basic acceleration and velocity.

--- src/ElmZero.elm
+++ src/ElmZero.elm
@@ -15,6 +15,8 @@ import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Window as Window
 import Task as Task
+import AnimationFrame exposing (diffs)
+import Time exposing (Time)
 
 type alias Position = {x: Float, y: Float}
 type alias Model =
@@ -23,27 +25,33 @@ type alias Model =
     , turnRate : Float
     , wWidth : Int
     , wHeight : Int
+    , xVelocity : Float
+    , yVelocity : Float
+    , acceleration : Float
     }
 
 initModel : Model
 initModel = { position = {x = 512.0, y = 0.0}
             , facing = 3 * pi / 2
-            , turnRate = pi / 30
+            , turnRate = pi / 60
             , wWidth = 1920
             , wHeight = 1080
+            , xVelocity = 0
+            , yVelocity = 0
+            , acceleration = 0.001
             }
 
-type Msg = UpPressed | DownPressed | LeftPressed | RightPressed | NothingPressed | WinSize (Int, Int)
+type Msg = UpPressed | DownPressed | LeftPressed | RightPressed | NothingPressed | WinSize (Int, Int) | Tick Time
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
         UpPressed ->
-            ( { model | position = {x = model.position.x - (cos model.facing), y = model.position.y - (sin model.facing)}}
+            ( { model | xVelocity = model.xVelocity - (cos model.facing) * model.acceleration, yVelocity = model.yVelocity - (sin model.facing) * model.acceleration}
             , Cmd.none
             )
         DownPressed ->
-            ( { model | position = {x = model.position.x + (cos model.facing), y = model.position.y + (sin model.facing)}}
+            ( { model | xVelocity = model.xVelocity + (cos model.facing) * model.acceleration, yVelocity = model.yVelocity + (sin model.facing) * model.acceleration}
             , Cmd.none
             )
         LeftPressed ->
@@ -62,21 +70,26 @@ update msg model =
             ( { model | wWidth = w, wHeight = h}
             , Cmd.none
             )
+        Tick time ->
+            ( { model | position = {x = model.position.x + (model.xVelocity * time), y = model.position.y + (model.yVelocity * time)}}
+            , Cmd.none
+            )
 
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.batch
-        [   Keyboard.downs
-                (
-                \kc -> case kc of
-                    37 -> RightPressed
-                    38 -> UpPressed
-                    39 -> LeftPressed
-                    40 -> DownPressed
-                    _ -> NothingPressed
-                )
-        ,   Window.resizes (\size -> (WinSize (size.width, size.height)))
+        [ Keyboard.downs
+            (
+            \kc -> case kc of
+                37 -> RightPressed
+                38 -> UpPressed
+                39 -> LeftPressed
+                40 -> DownPressed
+                _ -> NothingPressed
+            )
+        , Window.resizes (\size -> (WinSize (size.width, size.height)))
+        , diffs Tick
         ]
 
 winsizeToMsg : Window.Size -> Msg
@@ -87,7 +100,7 @@ view model =
       svg
         [ version "1.1", x "0", y "0", width (toString <| model.wWidth - 10), height (toString <| model.wHeight - 10)
         ]
-        [   g [transform ("translate(" ++ (toString ((toFloat model.wWidth) / 2)) ++ " " ++ (toString model.wHeight) ++ ") rotate(" ++ (toString -((model.facing * 180 / pi) - 90)) ++ ") translate(" ++ (toString (-model.position.x)) ++ " " ++ (toString (-model.position.y)) ++ ")")]
+        [ g [transform ("translate(" ++ (toString ((toFloat model.wWidth) / 2)) ++ " " ++ (toString model.wHeight) ++ ") rotate(" ++ (toString -((model.facing * 180 / pi) - 90)) ++ ") translate(" ++ (toString (-model.position.x)) ++ " " ++ (toString (-model.position.y)) ++ ")")]
             [   image [x "0", y "0", width "1024px", height "1024px", xlinkHref "../resources/lava.png"] []
             ]
         ]
@@ -96,8 +109,8 @@ view model =
 main : Program Never Model Msg
 main =
     Html.program
-    {   init = (initModel, Task.perform winsizeToMsg Window.size)
-    ,   subscriptions = subscriptions
-    ,   update = update
-    ,   view = view
+    { init = (initModel, Task.perform winsizeToMsg Window.size)
+    , subscriptions = subscriptions
+    , update = update
+    , view = view
     }
