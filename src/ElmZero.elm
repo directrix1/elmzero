06e6c235d6d2b3343f3b372b6be41e9335d6df07
Edward Flick <directrix1@gmail.com>
2017-01-17 03:29:14 -0600

    Track rotates and top is forward.

--- src/ElmZero.elm
+++ src/ElmZero.elm
@@ -13,21 +13,27 @@ import Platform.Cmd as Cmd
 import Platform.Sub as Sub
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
+import Window as Window
+import Task as Task
 
 type alias Position = {x: Float, y: Float}
 type alias Model =
     { position : Position
     , facing : Float
     , turnRate : Float
+    , wWidth : Int
+    , wHeight : Int
     }
 
 initModel : Model
-initModel = { position = {x = 0.0, y = 0.0}
-            , facing = pi * 3 / 2
+initModel = { position = {x = 512.0, y = 0.0}
+            , facing = pi / 2
             , turnRate = pi / 30
+            , wWidth = 1920
+            , wHeight = 1080
             }
 
-type Msg = UpPressed | DownPressed | LeftPressed | RightPressed | NothingPressed
+type Msg = UpPressed | DownPressed | LeftPressed | RightPressed | NothingPressed | WinSize (Int, Int)
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -52,27 +58,37 @@ update msg model =
             ( model
             , Cmd.none
             )
+        WinSize (w, h) ->
+            ( { model | wWidth = w, wHeight = h}
+            , Cmd.none
+            )
 
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-    Keyboard.downs
-        (
-        \kc -> case kc of
-            37 -> RightPressed
-            38 -> UpPressed
-            39 -> LeftPressed
-            40 -> DownPressed
-            _ -> NothingPressed
-        )
+    Sub.batch
+        [   Keyboard.downs
+                (
+                \kc -> case kc of
+                    37 -> RightPressed
+                    38 -> UpPressed
+                    39 -> LeftPressed
+                    40 -> DownPressed
+                    _ -> NothingPressed
+                )
+        ,   Window.resizes (\size -> (WinSize (size.width, size.height)))
+        ]
+
+winsizeToMsg : Window.Size -> Msg
+winsizeToMsg size = WinSize (size.width, size.height)
 
 view : Model -> (Html Msg)
 view model =
       svg
-        [ version "1.1", x "0", y "0", width "100%", height "99%"
+        [ version "1.1", x "0", y "0", width (toString <| model.wWidth - 10), height (toString <| model.wHeight - 10)
         ]
-        [   g [rotate "1.0"]
-            [   image [x (toString model.position.x), y (toString model.position.y), width "1024px", height "1024px", xlinkHref "../resources/lava.png"] []
+        [   g [transform ("translate(" ++ (toString ((toFloat model.wWidth) / 2)) ++ " " ++ (toString model.wHeight) ++ ") rotate(" ++ (toString -((model.facing * 180 / pi) + 90)) ++ ") translate(" ++ (toString (-model.position.x)) ++ " " ++ (toString (-model.position.y)) ++ ")")]
+            [   image [x "0", y "0", width "1024px", height "1024px", xlinkHref "../resources/lava.png"] []
             ]
         ]
 
@@ -80,7 +96,7 @@ view model =
 main : Program Never Model Msg
 main =
     Html.program
-    {   init = (initModel, Cmd.none)
+    {   init = (initModel, Task.perform winsizeToMsg Window.size)
     ,   subscriptions = subscriptions
     ,   update = update
     ,   view = view
